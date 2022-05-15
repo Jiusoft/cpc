0dc39417c44ad04f156be3068ebee98a6a1129cc
orlandohuang <orlando.huang8@pm.me>
2022-05-15 21:45:07 +0700

    added libguimod and #addmod thingys :rofl:

--- main.py
+++ main.py
@@ -30,60 +30,78 @@ def checkindent(code, indent=0):
 
 
 def toPython(code):
-    if checkindent(code) is None:
-        indent = 0
-    else:
-        indent = checkindent(code)
-    to_return = ""
-    tmp = list(code)
-    tmp1 = []
-    for item in tmp:
-        if item != "\\":
-            tmp1.append(item)
-        else:
-            tmp1.append("escape")
-    for item in tmp1:
-        if item == "escape":
-            i = tmp1.index(item)
-            del tmp1[i]
-            del tmp1[i]
-    command_list = "".join(tmp1).split()
-    main = command_list[0]
-    del tmp, tmp1
-
-    if main == "putln":
-        to_return = "print(" + " ".join(command_list[1:]) + ")"
-
-    if main == "getinput":
-        to_return = "inputresult = input(" + " ".join(command_list[1:]) + ")"
-
-    if "=" in command_list:
-        to_return = " ".join(command_list)
-
-    if main == "IF":
-        if not command_list[-1] == "THEN":
-            to_return = 'print(\"\\"THEN\\" expected\")'
-        else:
-            condition = command_list[1:-1]
-            if len(condition) != 3 or not (condition[1] in ["<", "<=", "==", ">=", ">"]):
-                to_return = 'print("If then condition syntax must be: \\"IF <var> </<=/==/>=/> <var> THEN")'
+    if code == "\n":
+        return ""
+    elif code.startswith("#addmod "):
+        try:
+            if code.split(" ")[1] == "libguimod\n":
+                return "import tkinter as tk"
             else:
-                to_return = 'if ' + " ".join(condition) + ":"
-    for i in range(indent):
-        to_return = "\t" + to_return
-    return to_return
+                return 'print(\"ERROR: No such module!\")'
+        except IndexError:
+            return 'print(\"ERROR: Syntax for adding module is \\"#addmod MODULE\\"\"'
+
+    else:
+        if checkindent(code) is None:
+            indent = 0
+        else:
+            indent = checkindent(code)
+        to_return = ""
+        tmp = list(code)
+        tmp1 = []
+        for item in tmp:
+            if item != "\\":
+                tmp1.append(item)
+            else:
+                tmp1.append("escape")
+        for item in tmp1:
+            if item == "escape":
+                i = tmp1.index(item)
+                del tmp1[i]
+                del tmp1[i]
+        command_list = "".join(tmp1).split()
+        main = command_list[0]
+        del tmp, tmp1
+
+        if main == "putln":
+            to_return = "print(" + " ".join(command_list[1:]) + ")"
+
+        if main == "getinput":
+            to_return = "inputresult = input(" + " ".join(command_list[1:]) + ")"
+
+        if "=" in command_list:
+            to_return = " ".join(command_list)
+
+        if main == "IF":
+            if not command_list[-1] == "THEN":
+                to_return = 'print(\"ERROR: \\"THEN\\" expected\")'
+            else:
+                condition = command_list[1:-1]
+                if len(condition) != 3 or not (condition[1] in ["<", "<=", "==", ">=", ">"]):
+                    to_return = 'print(\"ERROR: If then condition syntax must be: \\"IF <var> </<=/==/>=/> <var> ' \
+                                'THEN\\"\") '
+                else:
+                    to_return = 'if ' + " ".join(condition) + ":"
+        for i in range(indent):
+            to_return = "\t" + to_return
+        return to_return
 
 
 if len(args) == 1:
     filename = args[0].split("/")[-1].split(".")[0]
     with open(filename + ".py", 'a+') as f:
         f.write(
-            "import socket\nimport os\nimport math\nimport sys\nhostname=socket.gethostname()\nhostip=socket.gethostbyname(hostname)\n")
+            "import socket\nimport os\nimport math\nimport sys\nhostname=socket.gethostname("
+            ")\nhostip=socket.gethostbyname(hostname)\n")
     with open(args[0], 'r') as f:
         for line in f.readlines():
             toAppend = toPython(line)
             with open(filename + ".py", 'a') as nf:
                 nf.write(toAppend + "\n")
+
+    with open(filename + ".py") as f:
+        print(f.read())
+        print("\n\n")
 
     compiletoSystem = Popen(["python3", "-m", "PyInstaller", filename + ".py", "--onefile", "--distpath", "."])
     compiletoSystem.wait()
