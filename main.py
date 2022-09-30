cc77ced61a1dc5c1ad4c41797a2f7c71005aca1c
orlandohuang <orlando.huang8@pm.me>
2022-09-30 20:57:24 +0700

    license

--- main.py
+++ main.py
@@ -5,7 +5,30 @@ global funcs
 funcs = []
 
 def help():
-    print('cpc version 2.0.0\nCopyright (c) 2022 Jiusoft\nUsage: cpc <filename>\n\nArguments:\n\t-h, -H or --help: Display this help message')
+    print('cpc version 2.0.0\nCopyright (c) 2022 Jiusoft\nUsage: cpc <filename>\n\nArguments:\n\t-h, -H or --help: Display this help message\n\t-l, -L or --license: Display License Agreement')
+
+def license():
+    print("""MIT License
+
+Copyright (c) 2022 Jiusoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.""")
 
 def compile():
     global origfilename
@@ -230,10 +253,13 @@ def toPython(code):
 if len(args)==0:
     print('Required Argument: <filename>')
     print('Use -h, -H, or --help for help. ')
+    print('Use -l, -L, or --license for license agreement. ')
     sys.exit(1)
 elif len(args)==1:
     if args[0]=='-h' or args[0]=='-H' or args[0]=='--help':
         help()
+    if args[0]=='-l' or args[0]=='-L' or args[0]=='--license':
+        license()
     else:
         compile()
 else:
